25e5d3b965d6816fad6f85c3936438b1ff4721cf
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-05-02 08:16:10 +0900

    repair test

--- test/log.hs
+++ test/log.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds, TypeOperators #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs -fplugin=Plugin.TypeCheck.Nat.Simple #-}
 
 import GHC.TypeNats
@@ -11,8 +12,11 @@ foo = id
 bar :: 1 <= a => Proxy (a + 1 - 1) -> Proxy (a - 1 + 1)
 bar = id
 
-baz :: Proxy (a + b) -> Proxy ((a + 1) + (b - 1))
+baz :: 1 <= b => Proxy (a + b) -> Proxy ((a + 1) + (b - 1))
 baz = id
+
+hoge :: Proxy (a - 1) -> Proxy (a - 1)
+hoge = id
 
 main :: IO ()
 main = putStrLn "test log"
